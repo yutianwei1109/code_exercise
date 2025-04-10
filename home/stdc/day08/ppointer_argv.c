689ab35cd6c1b9941cf3bcb11dcc0fc5533a1ef2
yutianwei1109 <william837809246@yahoo.com>
2025-04-10 11:09:14 +0800

    Update ppointer_argv.c

--- home/stdc/day08/ppointer_argv.c
+++ home/stdc/day08/ppointer_argv.c
@@ -8,7 +8,15 @@ int main(int argc, char* argv[])//char* argv[]等价于char** argv
     {
         printf("%s\n", argv[i]);
     }
-    printf("%lu\n", sizeof(argv));//char*[]类型的数组名等价于char**的指针
+    //char*[]类型的数组作为函数参数时会退化为char**类型的的指针
+    /*
+    VScode Warning
+    warning: 'sizeof' on array function parameter 'argv' will return size of 'char **' [-Wsizeof-array-argument]
+        printf("sizeof(argv)[main?= %lu\n", sizeoff(argv));
+    */
+    printf("sizeof(argv) = %lu\n", sizeof(argv));//sizeof(argv) = 8
+    char* ch[4];
+    printf("sizeof(ch) = %lu\n", sizeof(ch));//sizeof(ch) = 32
 
     return 0;
 }
