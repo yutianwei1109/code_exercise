6614d7fb8a7f669cece950b93f4a5cb1ad800d3a
yutianwei1109 <william837809246@yahoo.com>
2025-04-10 11:08:57 +0800

    Create ppointer_swap.c

---
+++ home/stdc/day08/ppointer_swap.c
@@ -0,0 +1,19 @@
+//二级指针练习
+#include <stdio.h>
+
+//对二级指针对应的存储区进行交换操作
+void swap_pointer(char** pp1, char** pp2)
+{
+    char* ptmp = *pp1;
+    *pp1 = *pp2, *pp2 = ptmp;
+}
+
+int main()
+{
+    char* p1 = "hello";
+    char* p2 = "world";
+    swap_pointer(&p1, &p2);
+    printf("%s %s\n", p1, p2);
+
+    return 0;
+}
