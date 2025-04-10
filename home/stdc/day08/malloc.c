af844822e32e5e3b6446bd5cdecf6a1a27c360ae
yutianwei1109 <william837809246@yahoo.com>
2025-04-10 15:33:46 +0800

    Create malloc.c

---
+++ home/stdc/day08/malloc.c
@@ -0,0 +1,23 @@
+//动态内存分配
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int* p = malloc(8);//malloc不会初始化存储区
+    if(p == NULL)
+    {
+        printf("内存分配失败\n");
+        return -1;
+    }
+
+    *p = 10;
+    *(p + 1) = 20;
+
+    printf("%d %d", p[0], p[1]);
+
+    free(p);//释放内存
+    p = NULL;//指针置空
+
+    return 0;
+}
